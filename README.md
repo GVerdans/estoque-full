23ce85e850a9ab74964ffbff53113ca6bf69c084
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-07-11 20:54:20 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto Estoque
+## Fullstack
---
+++ estoque-front/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-gray-500 text-3xl">
+      <h1>Estoque</h1>
+    </div>
+  );
+}
+
+export default App;

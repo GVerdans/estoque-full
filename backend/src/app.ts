c6fdab48c53501aed79072a4a84c518598db7535
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-07-12 22:38:20 -0300

    adjusts in files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { type Express, type Request, type Response } from "express";
 import router from "./routes/index.routes";
 
 const app: Express = express();
+app.use(express.json());
 app.use(router);
 
 export default app;
--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,14 +1,12 @@
-import { Router } from "express";
-import app from "../app";
-import { type Request, type Response } from "express";
+import { Router, type Request, type Response } from "express";
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
       res.json({
-            Status: "Online",
-            Version: "1.0",
-            Author: "Gabriel Verdan",
+            status: "Online",
+            version: "1.0",
+            author: "Gabriel Verdan",
       });
 });
 

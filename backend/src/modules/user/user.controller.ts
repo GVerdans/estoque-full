4469d390d32cdee78fa87b9060a722761a49b5d7
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-07-14 19:58:58 -0300

    user module done

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -2,6 +2,25 @@ import { Request, Response } from "express";
 import { findAllUsers } from "./user.service";
 
 export async function getUsers(req: Request, res: Response) {
-      const users = await findAllUsers();
-      res.status(200).json(users);
+      try {
+            const users = await findAllUsers();
+            if (users.length == 0) {
+                  res.status(200).json({
+                        message: "A lista de usuarios está vazia !",
+                  });
+            }
+
+            const sanitazedUsers = users.map((user) => ({
+                  nome: user.name,
+                  email: user.email,
+            }));
+
+            res.status(200).json({
+                  usuarios: sanitazedUsers,
+            });
+      } catch (err) {
+            res.status(500).json({
+                  err: "Erro ao buscar usuarios ",
+            });
+      }
 }

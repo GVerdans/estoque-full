893d8ed73c396085913948b7401b6115663fb40a
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-07-20 14:01:10 -0300

    configurado cors no app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,17 @@
 import express, { type Express, type Request, type Response } from "express";
+import cors from "cors";
 import router from "./routes/index.routes.js";
 
 const app: Express = express();
+
+// Mudar quando for pra prod
+app.use(
+      cors({
+            origin: "http://localhost:5173",
+            credentials: true,
+      }),
+);
+
 app.use(express.json());
 app.use(router);
 

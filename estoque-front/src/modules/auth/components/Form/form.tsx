b92ee9ee6b17c2d5da9f7721fa43ba4c10b79c00
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-07-21 14:32:24 -0300

    loginForm falta o service

--- estoque-front/src/modules/auth/components/Form/form.tsx
+++ estoque-front/src/modules/auth/components/Form/form.tsx
@@ -1,26 +1,43 @@
+import { useState } from "react";
 import Button from "../../../../components/Button/Button";
 import Input from "../../../../components/inputs/Input";
 
 export default function LoginForm() {
+      const [email, setEmail] = useState("");
+      const [password, setPassword] = useState("");
+
+      function handleSubmit(el: React.FormEvent<HTMLFormElement>) {
+            el.preventDefault();
+            // service aqui ...
+      }
+
       return (
             <>
-                  <form>
-                        <div className="flex flex-col align-center items-center">
+                  <form onSubmit={handleSubmit}>
+                        <div className="flex flex-col align-center items-center my-10">
                               <div className="w-1/2">
                                     <Input
                                           label="Email"
                                           placeholder="email@email.com"
                                           type="email"
+                                          value={email}
+                                          onChange={(el) =>
+                                                setEmail(el.target.value)
+                                          }
                                     />
                                     <Input
                                           label="Senha"
                                           placeholder="******"
                                           type="password"
+                                          value={password}
+                                          onChange={(el) =>
+                                                setPassword(el.target.value)
+                                          }
                                     />
                                     <div className="flex flex-row justify-between items-end py-2">
-                                          <Button>Entrar</Button>
+                                          <Button type="submit">Entrar</Button>
                                           <div className="registre-se">
-                                                <p>
+                                                <p className="text-sm">
                                                       Nao possui uma conta ?
                                                       Registre-se aqui !
                                                 </p>

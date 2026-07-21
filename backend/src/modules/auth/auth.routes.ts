14d190245ba3075d64aa2cdddbee7da1d3e1fd9d
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-07-21 19:30:51 -0300

    alterado rota de createUser para /auth

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { loginController, changePasswordController } from "./auth.controller";
+import { createUserController } from "../user/user.controller";
 import { authMiddleware } from "../../middleware/auth.middleware";
 
 const router = Router();
 
 // POST
 router.post("/login", loginController);
+router.post("/register", createUserController);
 
 // PATCH
 router.patch("/change-password/:id", authMiddleware, changePasswordController);
--- backend/src/modules/user/user.routes.ts
+++ backend/src/modules/user/user.routes.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import { getUsers, createUserController } from "./user.controller";
+import { getUsers } from "./user.controller";
 import { authMiddleware } from "../../middleware/auth.middleware";
 
 const router = Router();
 
 // GET
 router.get("/", authMiddleware, getUsers);
-
-// POST
-router.post("/", createUserController);
 
 export default router;

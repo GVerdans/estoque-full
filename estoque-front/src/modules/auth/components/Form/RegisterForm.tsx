6a8436fcfe7ce0ff87f65c05c24e16e3cd11b84a
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-07-22 15:29:09 -0300

    terminado modulo de registro, falta enfeitar. React Toastfy eh uma ideia

--- estoque-front/src/modules/auth/components/Form/RegisterForm.tsx
+++ estoque-front/src/modules/auth/components/Form/RegisterForm.tsx
@@ -1,7 +1,78 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { registerService } from "../../auth.service";
+import BackButton from "../../../../components/Button/BackButton";
+import Button from "../../../../components/Button/Button";
+import Input from "../../../../components/inputs/Input";
+
 export default function RegisterForm() {
+      const [name, setNome] = useState<string>("");
+      const [email, setEmail] = useState<string>("");
+      const [password, setPassword] = useState<string>("");
+      const [err, setErr] = useState<string | null>(null);
+
+      const navigate = useNavigate();
+
+      async function handleSubmit(el: React.SubmitEvent) {
+            el.preventDefault();
+            try {
+                  const data = await registerService(name, email, password);
+                  if (data) {
+                        navigate("/");
+                  }
+            } catch (err) {
+                  setErr(
+                        err instanceof Error
+                              ? err.message
+                              : "Erro ao criar Usuário !",
+                  );
+            }
+      }
+
       return (
             <>
-                  <h1>REGISTER</h1>
+                  <form onSubmit={handleSubmit}>
+                        <div className="flex flex-col md:justify-center md:items-center">
+                              <div className="md:w-1/2">
+                                    <Input
+                                          label="Nome:"
+                                          type="text"
+                                          placeholder="Nome"
+                                          value={name}
+                                          onChange={(el) =>
+                                                setNome(el.target.value)
+                                          }
+                                    />
+                                    <Input
+                                          label="Email:"
+                                          type="email"
+                                          placeholder="email@email.com"
+                                          value={email}
+                                          onChange={(el) =>
+                                                setEmail(el.target.value)
+                                          }
+                                    />
+                                    <Input
+                                          label="Senha:"
+                                          type="password"
+                                          placeholder="Sua senha precisa ter um minino de 6 caracteres"
+                                          value={password}
+                                          onChange={(el) =>
+                                                setPassword(el.target.value)
+                                          }
+                                    />
+                              </div>
+                              {err && (
+                                    <div className="text-sm text-red-500">
+                                          {err}
+                                    </div>
+                              )}
+                        </div>
+                        <div className="flex flex-row justify-between py-4 md:justify-evenly">
+                              <Button type="submit">Cadastrar</Button>
+                              <BackButton type="button" />
+                        </div>
+                  </form>
             </>
       );
 }
--- estoque-front/src/modules/auth/pages/Register.tsx
+++ estoque-front/src/modules/auth/pages/Register.tsx
@@ -1,12 +1,10 @@
 import RegisterForm from "../components/Form/RegisterForm";
 import Header from "../../../components/Header/Header";
-import BackButton from "../../../components/Button/BackButton";
 
 export default function RegisterPage() {
       return (
             <>
-                  <Header texto="Register" />
-                  <BackButton />
+                  <Header texto="Registre-se" />
                   <RegisterForm />
             </>
       );

6259a95d07b78e36e4fc1d51e8c4b67041f1fbd9
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-07-23 12:46:03 -0300

    add logout button in NavBar

--- estoque-front/src/components/Navbar/NavBar.tsx
+++ estoque-front/src/components/Navbar/NavBar.tsx
@@ -1,7 +1,17 @@
+import { useAuth } from "../../contexts/AuthContext";
+import Button from "../Button/Button";
+
 export default function NavBar() {
+      const { user, logout } = useAuth();
+
       return (
             <header className="flex h-16 items-center justify-between border-b border-accent px-6">
                   <h1 className="text-xl font-bold text-accent">Estoque</h1>
+                  {user && (
+                        <Button variant="danger" onClick={logout}>
+                              Sair
+                        </Button>
+                  )}
             </header>
       );
 }
